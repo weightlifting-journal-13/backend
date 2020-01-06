4e8cb57f44045181ae4f640f3a6b8fa8aa6d1932
Nathaniel Mosco <natemosco@gmail.com>
2020-01-06 15:10:50 -0500

    records table created

--- database/migrations/20200106142528_05.js
+++ database/migrations/20200106142528_05.js
@@ -1,8 +1,41 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("records", table => {
+    table.integer("records_id").increments();
+    table
+      .integer("exercise_id")
+      .notNullable()
+      .references("exercise_id")
+      .inTable("exercise")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .varchar("rest_time", 20)
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .integer("sets", 2)
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .integer("reps", 2)
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .varchar("weight", 20)
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .integer("suggested_order", 2)
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("records");
 };
