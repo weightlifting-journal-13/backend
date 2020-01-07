41e0bc11b12441f68641cb0a9cb877391b955ac8
Nathaniel Mosco <natemosco@gmail.com>
2020-01-06 22:13:23 -0500

    created jump table for records and workouts

--- database/migrations/20200106142532_06.js
+++ database/migrations/20200106142532_06.js
@@ -1,8 +1,23 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("workouts_records_jump", table => {
+    table
+      .integer("workout_id")
+      .references("workout_id")
+      .inTable("workouts")
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    table
+      .integer("records_id")
+      .references("records_id")
+      .inTable("records")
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("workouts_records_jump");
 };
